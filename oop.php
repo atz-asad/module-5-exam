968ede7fad1e0f152c7e5510c3827a833294b941
asad <ashad731386@gmail.com>
2025-01-06 22:13:19 +0600

    update

--- oop.php
+++ oop.php
@@ -1,85 +1,89 @@
 <?php
 
-class Book
-{
+class Book {
     private $title;
-    private $member;
     private $availableCopies;
 
-    public function __construct($title, $member, $availableCopies)
-    {
+    public function __construct($title, $availableCopies) {
         $this->title = $title;
-        $this->member = $member;
         $this->availableCopies = $availableCopies;
     }
 
-    public function getTitle()
-    {
+    public function getTitle() {
         return $this->title;
     }
 
-    public function getAvailableCopies()
-    {
+    public function getAvailableCopies() {
         return $this->availableCopies;
     }
 
-    public function borrowBook($member, $copies)
-    {
-        if ($this->availableCopies >= $copies) {
-            $this->availableCopies -= $copies;
-            $this->member = $member;
+    public function borrowBook() {
+        if ($this->availableCopies > 0) {
+            $this->availableCopies--;
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
-    public function returnBook()
-    {
+    public function returnBook() {
         $this->availableCopies++;
     }
 }
 
-class Member
-{
+class Member {
     private $name;
+    private $borrowedBooks = [];
 
-    public function __construct($name)
-    {
+    public function __construct($name) {
         $this->name = $name;
     }
 
-    public function getName()
-    {
+    public function getName() {
         return $this->name;
+    }
+
+    public function borrowBook($book) {
+        if ($book->borrowBook()) {
+            $this->borrowedBooks[] = $book;
+            return true;
+        }
+        return false;
+    }
+
+    public function returnBook($book) {
+        foreach ($this->borrowedBooks as $key => $borrowedBook) {
+            if ($borrowedBook->getTitle() === $book->getTitle()) {
+                unset($this->borrowedBooks[$key]);
+                $book->returnBook();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getBorrowedBooks() {
+        return $this->borrowedBooks;
     }
 }
 
-// Create books with available copies
-$allBooks = [
-    new Book("The Great Gatsby", null, 5),
-    new Book("To Kill a Mockingbird", null, 3),
-    new Book("History of Bangladesh 2024", null, 10),
-];
+// Create books
+$book1 = new Book("The Great Gatsby", 5);
+$book2 = new Book("To Kill a Mockingbird", 3);
+$book3 = new Book("The Crucible", 20);
 
 // Create members
 $member1 = new Member("John Doe");
 $member2 = new Member("Jane Smith");
-$member3 = new Member("Rahim Uddin");
+$member3 = new Member("Arthur Miller");
 
-// Borrow books
-$allBooks[0]->borrowBook($member1, 1);
-$allBooks[1]->borrowBook($member2, 1);
-$allBooks[2]->borrowBook($member3, 3);
+// Members borrow books
+$member1->borrowBook($book1);
+$member2->borrowBook($book2);
+$member3->borrowBook($book3);
 
-// Display available copies of all books
-foreach ($allBooks as $book) {
-    echo "Available copies of " . $book->getTitle() . ": " . $book->getAvailableCopies() . "<br>";
-}
-
-
+// Display available copies after borrowing
+echo "Available Copies of '" . $book1->getTitle() . "': " . $book1->getAvailableCopies() . "<br>";
+echo "Available Copies of '" . $book2->getTitle() . "': " . $book2->getAvailableCopies() . "<br>";
+echo "Available Copies of '" . $book3->getTitle() . "': " . $book3->getAvailableCopies() . "<br>";
 
 ?>
-
-
-
